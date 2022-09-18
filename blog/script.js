a3ca643b3a8cc8f794b3bd0c9713985f599174c1
Mike Lowe <mikelowe72@gmail.com>
2022-09-18 15:09:48 +1200

    complete javascript technical blog

---
+++ blog/script.js
@@ -0,0 +1,9 @@
+let fruits = ['banana', 'pineapple', 'apple', 'pear']
+
+fruits[2] // returns 'apple'
+
+function baldMike() {
+  console.log('Mike is bald')
+}
+
+baldMike() // output 'Mike is bald'
